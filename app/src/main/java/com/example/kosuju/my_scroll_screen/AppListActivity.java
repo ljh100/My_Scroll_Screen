af21f5ef23dfec667a288de6685d95db6eee018f
xofserve <samgain69@gmail.com>
2019-02-26 12:50:28 -0500

    To bring it new AppListActivity.java file

--- app/src/main/java/com/example/kosuju/my_scroll_screen/AppListActivity.java
+++ app/src/main/java/com/example/kosuju/my_scroll_screen/AppListActivity.java
@@ -1,7 +1,12 @@
 package com.example.kosuju.my_scroll_screen;
 
+import java.util.List;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class AppListActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class AppListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_list);
+
+        //list 가져옴
+        ListView listview = (ListView) findViewById(R.id.list_view);
+        PackageManager pm = getPackageManager();
+
+        List<ApplicationInfo> infos = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+
+
     }
 }
