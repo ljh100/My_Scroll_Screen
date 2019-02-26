6d63268412ae0d2d3672b0c7f59efd3cdb20942f
xofserve <samgain69@gmail.com>
2019-02-26 12:45:13 -0500

    Add activity of AppListActivity with activity_app_list and resouce file of item_app to use it in activity_app_list

---
+++ app/src/main/java/com/example/kosuju/my_scroll_screen/AppListActivity.java
@@ -0,0 +1,13 @@
+package com.example.kosuju.my_scroll_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_list);
+    }
+}
